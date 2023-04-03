f3ef0bcda6779356ad1dc18d64e79bd9af44d01a
julie2501 <61940879+julie2501@users.noreply.github.com>
2023-04-04 02:18:19 +0530

    Update and rename pythoncode.py.txt to pythoncode.py

--- pythoncode.py
+++ pythoncode.py
@@ -1,4 +1,5 @@
-To create a web scraper to read articles from theverge.com using Python, we can use the BeautifulSoup library for parsing HTML and requests for fetching the web page. Here's a sample code that extracts the headline, author, date, and URL of each article on theverge.com and stores them in a CSV file and an SQLite database:
+
+#To create a web scraper to read articles from theverge.com using Python, we can use the BeautifulSoup library for parsing HTML and requests for fetching the web page. Here's a sample code that extracts the headline, author, date, and URL of each article on theverge.com and stores them in a CSV file and an SQLite database:
 
 
 import csv
@@ -51,4 +52,4 @@ conn.commit()
 conn.close()
 
 
-To run this script on a cloud service like AWS, we can create an EC2 instance and set up a cron job to run the script daily. We can also configure the instance to automatically save the CSV and SQLite files to an S3 bucket for backup and easy access.
+#To run this script on a cloud service like AWS, we can create an EC2 instance and set up a cron job to run the script daily. We can also configure the instance to automatically save the CSV and SQLite files to an S3 bucket for backup and easy access.
